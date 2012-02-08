04043d1254594117f65c08d7efd1504656ccd4fa
Takehito Tanabe <dateofrock@gmail.com>
2012-02-08 23:13:26 +0900

    Renamed from SimpleDBEntity to SimpleDBDomain

--- src/main/java/com/dateofrock/simpledbmapper/SimpleDBDomain.java
+++ src/main/java/com/dateofrock/simpledbmapper/SimpleDBDomain.java
@@ -27,8 +27,9 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-public @interface SimpleDBEntity {
+public @interface SimpleDBDomain {
 
+	@Deprecated
 	public static final int MAX_NUMBER_DIGITS = 10;
 
 	/**
